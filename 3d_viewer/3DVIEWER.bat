82232aa23c8a333e523455e2469d8a69edbf552e
Micah <micahincorporated@gmail.com>
2019-01-13 14:53:51 -0600

    Add files via upload

---
+++ 3d_viewer/3DVIEWER.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+title Running Cozmo 3D Viewer...
+ver
+echo (C) Copyright Microsoft Corp.
+echo.
+:cmd
+py c:\Users\Homeschool\Downloads\examples\examples\apps\3d_viewer.py
+%cmd%
+echo.
+goto cmd
